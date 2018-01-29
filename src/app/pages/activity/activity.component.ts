1c1cb3055f91c50f58fd4cbef66787841a7d7954
JFritzel <jfritzel@ROAM156.local>
2018-01-29 16:39:41 -0600

    Recent Activity & Announcement updates.  'read more' added, new parameter for recent-activity component, 'max-size' determines how long each activity is before adding 'read more'

--- src/app/pages/activity/activity.component.ts
+++ src/app/pages/activity/activity.component.ts
@@ -9,10 +9,14 @@ import { ActivityService } from '../../services/activity.service';
 export class ActivityComponent implements OnInit {
 
   public activities: any[];
+  loading: boolean;
 
   // The number of activities from each category to display
   @Input('pageSize')
   pageSize = 10; // Display 10 activities from each category by default.
+
+  @Input('maxSize') // The size (length) of each message before linking to the page of the message.
+  maxSize = 1000;
 
   constructor(public activityService: ActivityService) { }
 
@@ -21,11 +25,14 @@ export class ActivityComponent implements OnInit {
   }
 
   getRecentActivity() {
+    this.loading = true;
     this.activityService.get().subscribe(
       data => {
+        this.loading = false;
         this.activities = data;
       },
       error => {
+        this.loading = false;
         console.log('Error');
       });
   }
---
+++ src/app/pipes/shorten.pipe.ts
@@ -0,0 +1,33 @@
+/*
+  This library is part of danrevah / ngx-pipes
+*/
+
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({name: 'shorten'})
+export class ShortenPipe implements PipeTransform {
+  transform(input: string, length?: number, suffix?: string, wordBreak?: boolean): string;
+  transform(input: any, length?: number, suffix?: string, wordBreak?: boolean): any;
+
+  transform(text: any, length: number = 0, suffix: string = '', wordBreak: boolean = true): string {
+    if (!isString(text)) {
+      return text;
+    }
+
+    if (text.length > length) {
+      if (wordBreak) {
+        return text.slice(0, length) + suffix;
+      }
+
+      // tslint:disable-next-line:no-bitwise
+      if (!!~text.indexOf(' ', length)) {
+        return text.slice(0, text.indexOf(' ', length)) + suffix;
+      }
+    }
+    return text;
+  }
+}
+
+export function isString(value: any) {
+  return typeof value === 'string';
+}
