43e34bee80f2e715412fda5e0c3ff781d6d3feaa
JFritzel <jfritzel@ROAM156.local>
2018-03-14 14:27:19 -0500

    IE Attempt 2

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -33,13 +33,12 @@ import 'core-js/es6/regexp';
 import 'core-js/es6/map';
 import 'core-js/es6/weak-map';
 import 'core-js/es6/set';
-import 'core-js/es6/array';
-import 'core-js/es7';   // For includes, currently only on Resnet.
+import 'core-js/es7/array';  // For includes, currently only on Resnet.
 
 // import './css-var.polyfill';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
-// import 'classlist.js';  // Run `npm install --save classlist.js`.
+//import 'classlist.js';  // Run `npm install --save classlist.js`.
 
 /** Evergreen browsers require these. **/
 import 'core-js/es6/reflect';
