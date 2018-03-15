eecacf0a7b8bafbbd6aa93c5cc4500f700b292ad
Scott Near <snear@betternoi.com>
2018-03-15 14:39:13 -0500

    style fixes

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -24,8 +24,7 @@ export class ThemeService {
     '.primary.active {background-color: ' + primary + ' !important}' +
     '.primary:hover {background-color: ' + primary + ' !important}' +
     'h1 {color: ' + primary + ' !important}' +
-    '.ui-fileupload-choose {background-color: ' + primary + ' !important}' +
-    '.rematt {background-color: ' + primary + ' !important}';
+    '.ui-fileupload-choose {background-color: ' + primary + ' !important}';
 
     const node = document.createElement('style');
     node.innerHTML = str;
